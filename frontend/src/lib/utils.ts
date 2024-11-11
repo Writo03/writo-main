e446b47e313807ab325175f5cec280d164e13416
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-11-11 19:45:29 +0530

    many update

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isMobile() {
+  // Check if the browser supports the window.matchMedia method
+  if (window.matchMedia) {
+    // Create a media condition that targets mobile devices
+    const mobileQuery = window.matchMedia(
+      "(max-width: 767px), (pointer: coarse)",
+    );
+
+    // Return the matches property of the MediaQueryList object
+    return mobileQuery.matches;
+  }
+
+  // Fallback for browsers that don't support matchMedia
+  return window.innerWidth <= 767 || "ontouchstart" in window;
 }

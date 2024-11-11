75bcedea3a99ee3f6be2db5cff54d05772cbe61e
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-11-11 19:48:54 +0530

    fix

---
+++ frontend/src/components/Home/HomeSlider.tsx
@@ -0,0 +1,42 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { isMobile } from "@/lib/utils";
+
+function HeroSlider({ items }: { items: string[] }) {
+  return (
+    <Swiper
+      style={
+        {
+          "--swiper-navigation-color": "hsl(var(--primary))",
+          "--swiper-pagination-color": "hsl(var(--primary))",
+          "--swiper-navigation-size": isMobile() ? "25px" : "35px",
+        } as React.CSSProperties
+      }
+      autoplay={true}
+      pagination={{
+        clickable: true,
+      }}
+      loop={true}
+      navigation={true}
+      modules={[Pagination, Navigation, Autoplay]}
+      className="mySwiper h-full w-full"
+    >
+      {items.map((item, idx) => (
+        <SwiperSlide key={idx} className="group h-full w-full">
+          <img
+            src={item}
+            alt={`Slider ${idx}`}
+            className="h-full w-full object-cover object-center lg:transition-all lg:duration-700 lg:ease-in-out lg:group-hover:scale-[1.01]"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default HeroSlider;

bcb7bfccc575a736dbfe82c597b0cb5e025d39e5
cforChobar <sidhuashman02@gmail.com>
2024-11-18 09:53:38 +0530

    route to fetch quizes for admin and mentor

--- backend/src/routes/quiz.routes.js
+++ backend/src/routes/quiz.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
-import {createQuiz, deleteQuiz, getQuizById, getQuizes, updateQuiz} from "../controllers/quiz.controller.js"
+import {createQuiz, deleteQuiz, getQuizById, getQuizes, getQuizesAll, updateQuiz} from "../controllers/quiz.controller.js"
 
 const router = Router()
 
@@ -8,6 +8,7 @@ router.use(verifyJWT)
 
 router.route("/create-quiz").post(createQuiz)
 router.route("/get-quizes").get(getQuizes)
+router.route("/get-quizes-all").get(getQuizesAll)
 router.route("/get-quiz/:quizId")
 .get(getQuizById)
 .patch(updateQuiz)

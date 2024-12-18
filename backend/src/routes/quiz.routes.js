bd2b4397fea8ec518f57ee27722c83e743faec81
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-12-18 21:21:44 +0530

    backend update for showing testseries without login

--- backend/src/routes/quiz.routes.js
+++ backend/src/routes/quiz.routes.js
@@ -1,17 +1,26 @@
-import {Router} from "express"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
-import {createQuiz, deleteQuiz, getQuizById, getQuizes, getQuizesAll, updateQuiz} from "../controllers/quiz.controller.js"
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  createQuiz,
+  deleteQuiz,
+  getQuizById,
+  getQuizes,
+  getQuizesAll,
+  updateQuiz,
+} from "../controllers/quiz.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.use(verifyJWT)
+router.route("/get-quizes").get(getQuizes);
 
-router.route("/create-quiz").post(createQuiz)
-router.route("/get-quizes").get(getQuizes)
-router.route("/get-quizes-all").get(getQuizesAll)
-router.route("/get-quiz/:quizId")
-.get(getQuizById)
-.patch(updateQuiz)
-.delete(deleteQuiz)
+router.use(verifyJWT);
 
-export default router
+router.route("/create-quiz").post(createQuiz);
+router.route("/get-quizes-all").get(getQuizesAll);
+router
+  .route("/get-quiz/:quizId")
+  .get(getQuizById)
+  .patch(updateQuiz)
+  .delete(deleteQuiz);
+
+export default router;
